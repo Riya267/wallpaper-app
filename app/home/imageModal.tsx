abe61110291b503f1933a2d37e62046ea7af634d
Riya267 <riyacec05@gmail.com>
2024-06-15 00:10:27 +0530

    download and share handlers added in image modal

--- app/home/imageModal.tsx
+++ app/home/imageModal.tsx
@@ -2,13 +2,17 @@ import { theme } from '@/constants/theme';
 import { AntDesign } from '@expo/vector-icons';
 import { BlurView } from 'expo-blur';
 import { useRouter, useLocalSearchParams } from 'expo-router';
-import { StatusBar, StyleSheet, View, TouchableOpacity, Platform } from 'react-native';
+import { StatusBar, StyleSheet, View, TouchableOpacity, Platform, ActivityIndicator, Alert } from 'react-native';
 import { width } from '@/util/helper';
 import { Image } from 'expo-image';
+import * as fileSystem from 'expo-file-system'
+import { useState } from 'react';
+import { shareAsync } from 'expo-sharing';
 
 export default function ImageModal() {
   const router = useRouter();
   const { imageUrl, imageHeight, imageWidth  } = useLocalSearchParams<{ imageUrl: string; imageHeight: string; imageWidth: string }>();
+  const [ loadingState, setLoadingState ] = useState("");
 
   const getImageDimensions = () => {
     const aspectRatio = Number(imageHeight) / Number(imageWidth);
@@ -23,6 +27,35 @@ export default function ImageModal() {
       : { height: calculatedHeight, width: calculatedWidth };
   };
   
+  const filePath = `${fileSystem.documentDirectory}${imageUrl?.split('/').pop()}`
+
+  const handleShare = async () => {
+    setLoadingState("share")
+    const uri = await downloadImage();
+    if(uri) {
+      await shareAsync(uri);
+    }
+  }
+
+  const handleDownload = async () => {
+    setLoadingState("download")
+    const uri = await downloadImage();
+    if(uri) {
+      Alert.alert("Image downloaded successfully")
+      router.back();
+    }
+  }
+
+  const downloadImage = async () => {
+    try {
+      const { uri } = await fileSystem.downloadAsync(imageUrl, filePath)
+      setLoadingState("");
+      return uri;
+     } catch (error: any) {
+      setLoadingState(""); 
+      return null;
+   }
+  }
 
   return (
     <BlurView intensity={100} tint="dark" style={styles.blurView}>
@@ -38,18 +71,25 @@ export default function ImageModal() {
             style={[styles.image, getImageDimensions()]}
             transition={1000}
             contentFit='fill'
+            onLoad={() => setLoadingState("")}
           />
         </View>
         <View style={styles.iconContainer}>
           <TouchableOpacity onPress={() => router.back()} style={styles.iconButton}>
             <AntDesign name='close' size={30} color={"white"}/>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => {}} style={styles.iconButton}>
-            <AntDesign name='download' size={30} color={"white"}/>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => {}} style={styles.iconButton}>
-            <AntDesign name='sharealt' size={30} color={"white"}/>
-          </TouchableOpacity>
+            {
+              loadingState === "download" ? <ActivityIndicator color={"white"} style={styles.iconButton}/> :
+              <TouchableOpacity onPress={() => handleDownload()} style={styles.iconButton}>
+                <AntDesign name='download' size={30} color={"white"}/>
+              </TouchableOpacity>
+            }
+          {
+              loadingState === "share" ? <ActivityIndicator color={"white"} size={"small"} style={styles.iconButton}/> :
+              <TouchableOpacity onPress={() => handleShare()} style={styles.iconButton}>
+                <AntDesign name='sharealt' size={30} color={"white"}/>
+              </TouchableOpacity>
+          }
         </View>
       </View>
     </BlurView>
